821bd15160d9a7767f15c68f03f57e99f1c4b009
Umutoni-Rita <umurita37@gmail.com>
2025-02-26 07:28:00 +0200

    Firebase connection

--- android/app/src/main/java/com/galaxies/firebase/MainActivity.kt
+++ android/app/src/main/java/com/galaxies/firebase/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.todoapp
+package com.galaxies.firebase
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/galaxies/firebase/MainApplication.kt
+++ android/app/src/main/java/com/galaxies/firebase/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.todoapp
+package com.galaxies.firebase
 
 import android.app.Application
 import android.content.res.Configuration
