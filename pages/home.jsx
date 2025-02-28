701d4eaf0689f33bf7ff1509a73d456476af71f7
Umutoni-Rita <umurita37@gmail.com>
2025-02-28 14:53:58 +0200

    Updates UI

---
+++ pages/home.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import React, { useState } from 'react';
+
+
+export const Home = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    const signUp = () => {
+        
+    }
+
+    const signIn = () => {
+
+    }
+
+}
---
+++ pages/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, TextInput } from 'react-native';
+
+const Login = () => {
+
+    return(
+        <View>
+            <TextInput placeholder='Email' 
+            />
+        </View>
+    )
+}
+export default Login
